b4c9f41176f8e7e8be96ebb3c95a88d60178eb8c
badwolfzm <shahaf0502937740@gmail.com>
2024-09-30 17:49:11 +0300

    checking for errors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+// Enable error reporting for development
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "Hello, World!"; // Your existing code
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Core/Autoloader.php';
 
